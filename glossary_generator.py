4dd39474b88691e395bdafb090b833bfabd140ae
Aaron Yong <aaronyong7922@gmail.com>
2020-05-10 16:37:32 +0800

    removed tkinter window, minor fixes

--- glossary_generator.py
+++ glossary_generator.py
@@ -2,6 +2,7 @@ import xlrd
 import os
 import tkinter as Tk
 from tkinter import filedialog
+from pathlib import Path
 from docx import Document
 from docx.enum.style import WD_STYLE_TYPE
 from docx.shared import Inches
@@ -28,67 +29,65 @@ class GlossaryGenerator:
 
 
 class GlossaryExporter:
-    def __init__(self):
+    def __init__(self, filepath, glossary, font_family):
+        self.filepath = filepath
+        self.glossary = glossary
+        self.font_family = font_family
         self.document = Document()
+        self.format_docx()
         
-    def format_docx(self, font_family):
+    def format_docx(self):
         term_style = self.document.styles.add_style("Term", WD_STYLE_TYPE.PARAGRAPH)
-        term_style.font.name = font_family
+        term_style.font.name = self.font_family
         term_style.paragraph_format.line_spacing = Inches(1/6)
 
         value_style = self.document.styles["List"]
-        value_style.font.name = font_family
+        value_style.font.name = self.font_family
         value_style.paragraph_format.line_spacing = Inches(0.20)
         value_style.paragraph_format.left_indent = Inches(0.5)
         
 
-    def export_glossary(self, filepath, glossary, font_family):
-        self.format_docx(font_family)
-        terms = sorted(list(glossary.keys()), key = str.casefold)
+    def export_glossary(self):
+        terms = sorted(list(self.glossary.keys()), key = str.casefold)
         for term in terms:
-            values = sorted(list(glossary[term].keys()), key = str.casefold)
+            values = sorted(list(self.glossary[term].keys()), key = str.casefold)
             self.document.add_paragraph(style = "Term").add_run(term).bold = True
             for value in values:
-                topics_pgs = glossary[term][value]
+                topics_pgs = self.glossary[term][value]
                 value_para = self.document.add_paragraph(value, style = "List")
                 for topic_pg in topics_pgs:
                     value_para.add_run(f", ({topic_pg[0]}, {topic_pg[1]})")
                     
-        self.document.save(filepath)
+        self.document.save(self.filepath)
 
 
 class MainApp:
-    def __init__(self, parent):
-        self.parent = parent
+    def __init__(self):
         self.run()
 
     def run(self):
         curr_dir = os.getcwd()
         input_dir = filedialog.askopenfilename(initialdir = curr_dir, title = "Please select a file to convert to a glossary.")
-        output_dir = filedialog.asksaveasfilename(initialdir = curr_dir, title = "Please select a folder to save the glossary.", \
-                                                  defaultextension = ".docx", filetypes = (("Word Document", "*.docx"),))  
+        if not input_dir: return
+        
+        output_dir = filedialog.asksaveasfilename(initialdir = curr_dir, \
+                                                  title = "Please select a folder to save the glossary.", \
+                                                  initialfile = Path(input_dir).stem, \
+                                                  defaultextension = ".docx", \
+                                                  filetypes = (("Word Document", "*.docx"),))
+        
         self.glossarygenerator = GlossaryGenerator(input_dir)
+        self.glossaryexporter = GlossaryExporter(output_dir, self.glossarygenerator.glossary, "Calibri")
         self.glossarygenerator.convert_glossary()
-
-        self.glossaryexporter = GlossaryExporter()
-        self.glossaryexporter.export_glossary(output_dir, self.glossarygenerator.glossary, "Calibri")
-        self.parent.destroy()
-
-
-
+        self.glossaryexporter.export_glossary()
+        
 
 def main():
-    root = Tk.Tk()
-    MainApp(root)
-    root.mainloop()
+    Tk.Tk().withdraw()
+    MainApp()
 
 if __name__ == "__main__":
     main()
 
-# g = GlossaryGenerator("./2211_glossary.xlsx")
-# g.convert_glossary()
-
-# e = GlossaryExporter(".", "testing")
-# e.export_glossary(g.glossary, "Calibri")
 
 
