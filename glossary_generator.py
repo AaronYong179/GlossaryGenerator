6a3f932f5ec3287f78d973f0de61cc4aa067f7c1
Aaron Yong <aaronyong7922@gmail.com>
2020-05-10 14:18:01 +0800

    added GUI functionality

--- glossary_generator.py
+++ glossary_generator.py
@@ -1,4 +1,7 @@
 import xlrd
+import os
+import tkinter as Tk
+from tkinter import filedialog
 from docx import Document
 from docx.enum.style import WD_STYLE_TYPE
 from docx.shared import Inches
@@ -9,12 +12,15 @@ class GlossaryGenerator:
         self.sheet = xlrd.open_workbook(filepath).sheet_by_index(0)
 
     def _write(self, term, value, topic, page):
+        page = int(page)
+        term, value, topic, page = list(map(lambda x: str(x).strip(), (term, value, topic, page)))
+        
         if term not in self.glossary:
             self.glossary[term] = {}
         if value not in self.glossary[term]:
             self.glossary[term][value] = []
 
-        self.glossary[term][value].append((topic, int(page)))
+        self.glossary[term][value].append((topic, page))
 
     def convert_glossary(self):
         for row in range(1, self.sheet.nrows):
@@ -22,8 +28,7 @@ class GlossaryGenerator:
 
 
 class GlossaryExporter:
-    def __init__(self, filepath, name):
-        self.filepath = f"{filepath}/{name}.docx"
+    def __init__(self):
         self.document = Document()
         
     def format_docx(self, font_family):
@@ -31,32 +36,59 @@ class GlossaryExporter:
         term_style.font.name = font_family
         term_style.paragraph_format.line_spacing = Inches(1/6)
 
-        value_style = self.document.styles["List Bullet"]
+        value_style = self.document.styles["List"]
         value_style.font.name = font_family
         value_style.paragraph_format.line_spacing = Inches(0.20)
         value_style.paragraph_format.left_indent = Inches(0.5)
         
 
-    def export_glossary(self, glossary, font_family):
-        self.format_docx("Calibri")
-
+    def export_glossary(self, filepath, glossary, font_family):
+        self.format_docx(font_family)
         terms = sorted(list(glossary.keys()), key = str.casefold)
         for term in terms:
             values = sorted(list(glossary[term].keys()), key = str.casefold)
             self.document.add_paragraph(style = "Term").add_run(term).bold = True
             for value in values:
                 topics_pgs = glossary[term][value]
-                value_para = self.document.add_paragraph(value, style = "List Bullet")
+                value_para = self.document.add_paragraph(value, style = "List")
                 for topic_pg in topics_pgs:
                     value_para.add_run(f", ({topic_pg[0]}, {topic_pg[1]})")
                     
-        self.document.save(self.filepath)
+        self.document.save(filepath)
 
 
-##g = GlossaryGenerator("./2211_glossary.xlsx")
-##g.convert_glossary()
-##
-##e = GlossaryExporter(".", "testing")
-##e.export_glossary(g.glossary, "Calibri")
+class MainApp:
+    def __init__(self, parent):
+        self.parent = parent
+        self.run()
+
+    def run(self):
+        curr_dir = os.getcwd()
+        input_dir = filedialog.askopenfilename(initialdir = curr_dir, title = "Please select a file to convert to a glossary.")
+        output_dir = filedialog.asksaveasfilename(initialdir = curr_dir, title = "Please select a folder to save the glossary.", \
+                                                  defaultextension = ".docx", filetypes = (("Word Document", "*.docx"),))  
+        self.glossarygenerator = GlossaryGenerator(input_dir)
+        self.glossarygenerator.convert_glossary()
+
+        self.glossaryexporter = GlossaryExporter()
+        self.glossaryexporter.export_glossary(output_dir, self.glossarygenerator.glossary, "Calibri")
+        self.parent.destroy()
+
+
+
+
+def main():
+    root = Tk.Tk()
+    MainApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+# g = GlossaryGenerator("./2211_glossary.xlsx")
+# g.convert_glossary()
+
+# e = GlossaryExporter(".", "testing")
+# e.export_glossary(g.glossary, "Calibri")
 
 
